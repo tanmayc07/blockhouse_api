22f1fa2e96bbede5274a3d7de41d6a9bdca45b52
Tanmay Chaudhari <hctanmay@gmail.com>
2025-02-23 15:25:53 -0500

    Add routes to handle orders

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
+from database import Orders, engine, init_db
 
 app = FastAPI()
 
+'''
+Database Integration
+'''
+init_db()
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+'''
+Routes
+'''
 @app.get("/")
 async def root():
     return {"message": "Welcome to Blockhouse API"}
 
 @app.get("/orders")
-async def get_orders():
-    return {"orders": {1, 2, 3, 4}}
+async def get_orders(db: Session = Depends(get_db)):
+    return db.exec(select(Orders)).all()
+
+@app.post("/orders")
+async def post_order(order: Orders, db: Session = Depends(get_db)):
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+    return order
